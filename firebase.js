2da4decca82ffd1c651f35213d0d7ec860ac35fc
nguyenvanhuy28072000-hue <nguyenvanhuy28072000@gmail.com>
2026-06-11 16:59:00 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 
-import {
-  getFirestore,
-  doc,
-  setDoc,
-  getDoc,
-  onSnapshot
-} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBzlvVUxZrMaU8OxYve9GNts1ZdCz35CWk",
@@ -15,14 +7,14 @@ const firebaseConfig = {
   storageBucket: "kitchen-app-31fa9.firebasestorage.app",
   messagingSenderId: "18408555856",
   appId: "1:18408555856:web:b4a4337326d408f5f5d855",
-  measurementId: "G-FH002VEFFE"
+  
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// 初期化（compat版）
+firebase.initializeApp(firebaseConfig);
 
+// Firestore
+const db = firebase.firestore();
+
+// グローバル公開（iOS12対策）
 window.db = db;
-window.doc = doc;
-window.setDoc = setDoc;
-window.getDoc = getDoc;
-window.onSnapshot = onSnapshot;
