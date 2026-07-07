3d20f1d7ce555b79c153d44eb5cc4982ac121967
nguyenvanhuy28072000-hue <nguyenvanhuy28072000@gmail.com>
2026-07-07 13:56:44 +0900

    admin.js を作成

---
+++ admin.js
@@ -0,0 +1,67 @@
+window.db.collection("courses").get().then(snapshot=>{
+
+    const select=document.getElementById("courseSelect");
+
+    snapshot.forEach(doc=>{
+
+        select.innerHTML+=`
+        <option value="${doc.id}">
+            ${doc.id}
+        </option>
+        `;
+
+    });
+
+    loadCourse();
+
+});
+
+document.getElementById("courseSelect")
+.addEventListener("change",loadCourse);
+
+function loadCourse(){
+
+    const id=document.getElementById("courseSelect").value;
+
+    window.db.collection("courses")
+    .doc(id)
+    .get()
+    .then(doc=>{
+
+        const data=doc.data();
+
+        document.getElementById("duration").value=data.duration;
+
+        document.getElementById("dishes").value=
+        data.dishes.join("\n");
+
+    });
+
+}
+
+function saveCourse(){
+
+    const id=document.getElementById("courseSelect").value;
+
+    const duration=
+    Number(document.getElementById("duration").value);
+
+    const dishes=
+    document.getElementById("dishes")
+    .value
+    .split("\n")
+    .filter(x=>x!="");
+
+    window.db.collection("courses")
+    .doc(id)
+    .update({
+
+        duration:duration,
+
+        dishes:dishes
+
+    });
+
+    alert("保存しました");
+
+}
